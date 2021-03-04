582aac93833eb96aad3ce9028ad1a02209fa2d6b
fzy <1362975833@qq.com>
2021-03-04 14:10:33 +0800

    first commit

--- pre-ui/config/index.js
+++ pre-ui/config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/pre': {
-        target: 'http://localhost:8081',//后端接口地址
+        target: 'http://10.14.3.158:8081',//后端接口地址
         changeOrigin: true,  //是否跨域
         pathRewrite: {
           '^/pre': '/',//重写,
